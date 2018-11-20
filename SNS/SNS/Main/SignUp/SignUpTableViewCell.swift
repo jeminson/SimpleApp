c2665dee928c464efac935ae797085beedd1498b
Je Min Son <jeminson@Jeui-MacBook-Pro.local>
2018-11-20 12:54:44 -0600

    sign up page layout

---
+++ SNS/SNS/Main/SignUp/SignUpTableViewCell.swift
@@ -0,0 +1,26 @@
+//
+//  SignUpTableViewCell.swift
+//  SNS
+//
+//  Created by Je Min Son on 11/20/18.
+//  Copyright © 2018 Jason Son. All rights reserved.
+//
+
+import UIKit
+
+class SignUpTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var signUpTextField: UITextField!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
--- SNS/SNS/Main/SignUp/SignUpViewController.swift
+++ SNS/SNS/Main/SignUp/SignUpViewController.swift
@@ -10,13 +10,33 @@ import UIKit
 
 class SignUpViewController: MRKBaseViewController {
 
+    var placeHolderArray : [String] = ["First Name", "Last Name", "Email ID", "Address", "Phone Number", "Password"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         title = "Sign Up"
     }
     
+}
 
 
-
+extension SignUpViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return placeHolderArray.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "SignUpTableViewCell") as! SignUpTableViewCell
+        
+        cell.signUpTextField.placeholder = placeHolderArray[indexPath.row]
+        
+        if placeHolderArray[indexPath.row] == "Password" {
+            cell.signUpTextField.isSecureTextEntry = true
+        }
+        
+        return cell
+    }
+    
+    
 }
