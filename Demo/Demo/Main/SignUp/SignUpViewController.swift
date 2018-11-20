9e315fab99207b72f0635ad34b2c119322860950
Je Min Son <jeminson@Jeui-MBP.attlocal.net>
2018-11-20 01:57:56 -0600

    signup page temp done

--- Demo/Demo/Main/SignUp/SignUpViewController.swift
+++ Demo/Demo/Main/SignUp/SignUpViewController.swift
@@ -7,15 +7,26 @@
 //
 
 import UIKit
+import Firebase
+import TWMessageBarManager
 
 class SignUpViewController: UIViewController {
 
-    var placeHolderArray : [String] = ["First Name", "Last Name", "Email ID", "Address", "Phone Number", "City", "State", "Country", "Password", "Re-Enter Password"]
+    @IBOutlet weak var firstNameTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var phoneNumberTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
     
+    var datebaseReference: DatabaseReference!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         
         title = "SIGN UP"
+        
+        datebaseReference = Database.database().reference().child("USERS")
 
     }
     
@@ -25,23 +36,33 @@ class SignUpViewController: UIViewController {
         self.navigationController?.setNavigationBarHidden(false, animated: animated)
     }
 
-
-
+    @IBAction func submitActionButton(_ sender: UIButton) {
+        signUp(email: emailTextField.text!, passwd: passwordTextField.text!)
+    }
 }
 
-extension SignUpViewController: UITableViewDataSource {
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return placeHolderArray.count
+extension SignUpViewController {
+    func signUp(email: String, passwd: String) {
+        Auth.auth().createUser(withEmail: email, password: passwd) { (result, error) in
+            
+            if error == nil {
+                
+                guard let user = result?.user else {return}
+                
+                self.datebaseReference.child(user.uid).setValue(["ID":user.uid, "FirstName": self.firstNameTextField.text!, "LastName": self.lastNameTextField.text!,"EmailId":email, "Address":self.addressTextField.text!, "PhoneNumber":self.phoneNumberTextField.text!], withCompletionBlock: { (error, reference) in
+                    
+                    if error == nil{
+                        print(reference)
+                    }
+                    
+                })
+                TWMessageBarManager.sharedInstance().showMessage(withTitle: "Sucess", description: "Successfully register the new user", type: .success)
+            } else{
+                TWMessageBarManager.sharedInstance().showMessage(withTitle: "Error", description: error?.localizedDescription, type: .error)
+            }
+            
+        }
     }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "SignUpTableViewCell") as! SignUpTableViewCell
-        
-        cell.signUpTextField.placeholder = placeHolderArray[indexPath.row]
-        
-        return cell
-    }
-    
-    
 }
+
 
